7840af9ad19ffc34a7afac7e69e9134428accd14
ArerepadeB <aredelulu@gmail.com>
2021-11-01 09:30:12 -0500

    ALB setup for docker app

--- development/main.tf
+++ development/main.tf
@@ -11,56 +11,50 @@ resource "aws_instance" "server" {
 }
 
 # ###-------- ALB -------###
-# resource "aws_lb" "lb" {
-#   name               = join("-", [local.application.app_name, "lb"])
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.main-alb.id]
-#   subnets            = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
-#   idle_timeout       = "60"
+resource "aws_lb" "lb" {
+  name               = join("-", [local.application.app_name, "lb"])
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.main-alb.id]
+  subnets            = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
+  idle_timeout       = "60"
 
-#   access_logs {
-#     bucket  = aws_s3_bucket.logs_s3dev.bucket
-#     prefix  = join("-", [local.application.app_name, "lb-s3logs"])
-#     enabled = true
-#   }
-#   tags = merge(local.common_tags,
-#     { Name = "server"
-#   Application = "public" })
-# }
+  access_logs {
+    bucket  = aws_s3_bucket.logs_s3dev.bucket
+    prefix  = join("-", [local.application.app_name, "lb-s3logs"])
+    enabled = true
+  }
+  tags = merge(local.common_tags,
+    { Name = "server"
+  Application = "public" })
+}
 
-# ///////////////
-# resource "aws_wafv2_web_acl_association" "example" {
-#   resource_arn = aws_lb.lb.arn
-#   web_acl_arn  = aws_wafv2_web_acl.test.arn
-# }
+###------- ALB Health Check -------###
+resource "aws_lb_target_group" "app_tglb" {
+  name     = join("-", [local.application.app_name, "apptglb"])
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
 
-# ###------- ALB Health Check -------###
-# resource "aws_lb_target_group" "app_tglb" {
-#   name     = join("-", [local.application.app_name, "apptglb"])
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = aws_vpc.main.id
+  health_check {
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    healthy_threshold   = "5"
+    unhealthy_threshold = "2"
+    timeout             = "5"
+    interval            = "30"
+    matcher             = "200"
+  }
+}
 
-#   health_check {
-#     path                = "/"
-#     port                = "traffic-port"
-#     protocol            = "HTTP"
-#     healthy_threshold   = "5"
-#     unhealthy_threshold = "2"
-#     timeout             = "5"
-#     interval            = "30"
-#     matcher             = "200"
-#   }
-# }
+resource "aws_lb_target_group_attachment" "app_tglbat" {
+  target_group_arn = aws_lb_target_group.app_tglb.arn
+  target_id        = aws_instance.server.id
+  port             = 80
+}
 
-# resource "aws_lb_target_group_attachment" "app_tglbat" {
-#   target_group_arn = aws_lb_target_group.app_tglb.arn
-#   target_id        = aws_instance.server.id
-#   port             = 80
-# }
-
-# ####-------- SSL Cert ------#####
+####-------- SSL Cert ------#####
 # resource "aws_lb_listener" "app_lblist2" {
 #   load_balancer_arn = aws_lb.lb.arn
 #   port              = "443"
@@ -88,132 +82,132 @@ resource "aws_instance" "server" {
 #   }
 # }
 
-# ########------- S3 Bucket -----------####
-# resource "aws_s3_bucket" "logs_s3dev" {
-#   bucket = join("-", [local.application.app_name, "logdev"])
-#   acl    = "private"
+########------- S3 Bucket -----------####
+resource "aws_s3_bucket" "logs_s3dev" {
+  bucket = join("-", [local.application.app_name, "logdev"])
+  acl    = "private"
 
-#   tags = merge(local.common_tags,
-#     { Name = "server"
-#   bucket = "private" })
-# }
-# resource "aws_s3_bucket_policy" "logs_s3dev" {
-#   bucket = aws_s3_bucket.logs_s3dev.id
+  tags = merge(local.common_tags,
+    { Name = "server"
+  bucket = "private" })
+}
+resource "aws_s3_bucket_policy" "logs_s3dev" {
+  bucket = aws_s3_bucket.logs_s3dev.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Id      = "MYBUCKETPOLICY"
-#     Statement = [
-#       {
-#         Sid       = "Allow"
-#         Effect    = "Allow"
-#         Principal = "*"
-#         Action    = "s3:*"
-#         Resource = [
-#           aws_s3_bucket.logs_s3dev.arn,
-#           "${aws_s3_bucket.logs_s3dev.arn}/*",
-#         ]
-#         Condition = {
-#           NotIpAddress = {
-#             "aws:SourceIp" = "8.8.8.8/32"
-#           }
-#         }
-#       },
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "MYBUCKETPOLICY"
+    Statement = [
+      {
+        Sid       = "Allow"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.logs_s3dev.arn,
+          "${aws_s3_bucket.logs_s3dev.arn}/*",
+        ]
+        Condition = {
+          NotIpAddress = {
+            "aws:SourceIp" = "8.8.8.8/32"
+          }
+        }
+      },
+    ]
+  })
+}
 
-# #IAM
-# resource "aws_iam_role" "_role" {
-#   name = join("-", [local.application.app_name, "role"])
+#IAM
+resource "aws_iam_role" "_role" {
+  name = join("-", [local.application.app_name, "role"])
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Sid    = ""
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 
-#   tags = merge(local.common_tags,
-#     { Name = "server"
-#   Role = "role" })
-# }
+  tags = merge(local.common_tags,
+    { Name = "server"
+  Role = "role" })
+}
 
-# #######------- IAM Role ------######
-# resource "aws_iam_role_policy" "_policy" {
-#   name = join("-", [local.application.app_name, "policy"])
-#   role = aws_iam_role._role.id
+#######------- IAM Role ------######
+resource "aws_iam_role_policy" "_policy" {
+  name = join("-", [local.application.app_name, "policy"])
+  role = aws_iam_role._role.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = [
-#           "ec2:Describe*",
-#         ]
-#         Effect   = "Allow"
-#         Resource = "*"
-#       },
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
 
-# #####------ Certificate -----------####
-# resource "aws_acm_certificate" "cert" {
-#   domain_name       = "*.elietesolutionsit.de"
-#   validation_method = "DNS"
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-#   tags = merge(local.common_tags,
-#     { Name = "registration-app.elietesolutionsit.de"
-#   Cert = "cert" })
-# }
+#####------ Certificate -----------####
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "*.elietesolutionsit.de"
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = merge(local.common_tags,
+    { Name = "dockerapp.elietesolutionsit.de"
+  Cert = "cert" })
+}
 
-# ###------- Cert Validation -------###
-# data "aws_route53_zone" "main-zone" {
-#   name         = "elietesolutionsit.de"
-#   private_zone = false
-# }
+###------- Cert Validation -------###
+data "aws_route53_zone" "main-zone" {
+  name         = "elietesolutionsit.de"
+  private_zone = false
+}
 
-# resource "aws_route53_record" "zone_record" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
+resource "aws_route53_record" "zone_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 60
-#   type            = each.value.type
-#   zone_id         = data.aws_route53_zone.main-zone.zone_id
-# }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.main-zone.zone_id
+}
 
-# resource "aws_acm_certificate_validation" "cert" {
-#   certificate_arn         = aws_acm_certificate.cert.arn
-#   validation_record_fqdns = [for record in aws_route53_record.zone_record : record.fqdn]
-# }
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.zone_record : record.fqdn]
+}
 
-# ##------- ALB Alias record ----------##
-# resource "aws_route53_record" "www" {
-#   zone_id = data.aws_route53_zone.main-zone.zone_id
-#   name    = "registration-app.elietesolutionsit.de"
-#   type    = "A"
+##------- ALB Alias record ----------##
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.main-zone.zone_id
+  name    = "dockerapp.elietesolutionsit.de"
+  type    = "A"
 
-#   alias {
-#     name                   = aws_lb.lb.dns_name
-#     zone_id                = aws_lb.lb.zone_id
-#     evaluate_target_health = true
-#   }
-# }
+  alias {
+    name                   = aws_lb.lb.dns_name
+    zone_id                = aws_lb.lb.zone_id
+    evaluate_target_health = true
+  }
+}
