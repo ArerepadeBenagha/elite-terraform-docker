70a4bed2c66c146b660edfa242380a5a9f85aed5
ArerepadeB <aredelulu@gmail.com>
2021-11-01 09:13:12 -0500

    docker infra setup for dev

--- development/key.tf
+++ development/key.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "mykeypair" {
-  key_name   = "mykeypair"
-  public_key = file(var.path_to_public_key)
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCl2I1XayObf6pLf0I9U98xFjSIT3SyMeZrcaqc9ypAei0fAE/PLUlxET/qpNDooroOrRww+KST9x/GsN1wLh0glwteXv2s3xUv0ijWekfPrY8ernm1vx8W6B0tPN3y/oL4Gdw9zBX5QOqDVfAmT/U9k2PYvOPJLRkB7BkNL6Lc3bwnB/qcSEne42FjCzXy1A9rA5lD0tcrO/pE/bAcGa4PKZGQ72sc2NAopgx3l0H/EBqHDMcVCgqw2UId12IywZgaqiarqE+FYBrTuuRz6UdXK5+7Ja36ly77AL9tA/NBvi7RpXfiSKuokAVretJjFIrCJOpymJCfy1nQWUnqf9WBMRyEBMPuOdgAbI1xBr5c0H4JtMfGKGjd/qAXjyp68MAAY7uB4QAIIufVUxrWQw/I3PfOE+bDEkE1v/e5bwBZ1ZvXBj/GBs0AfwRkspBNe/Re1o3VAI9CNY6EKlBj5Y0wOqujsDrV7G6mCKOM/mDJr9vlTe2VXXO3XIFQMylWJDE= lbena@LAPTOP-QB0DU4OG"
 }
--- development/variables.tf
+++ development/variables.tf
@@ -1,11 +1,3 @@
-variable "path_to_public_key" {
-  description = "public key"
-  default     = "dockerkey.pub"
-}
 variable "instance_type" {
   description = "instance size for docker server"
-}
-variable "path" {
-  description = "private key"
-  default     = "dockerkey.pem"
 }
